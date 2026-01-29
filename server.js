146930e42caf12bdf50dbff838a6a96ed736105c
satoru2478-wq <satoru2478@gmail.com>
2026-01-29 21:25:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import { WebSocketServer } from "ws";
 const PORT = process.env.PORT || 3000;
 const wss = new WebSocketServer({ port: PORT });
 
-wss.on("connection", ws => {
-  ws.on("message", msg => {
-    // broadcast to everyone except sender
-    wss.clients.forEach(client => {
+wss.on("connection", (ws) => {
+  ws.on("message", (msg) => {
+    // Broadcast message to everyone else
+    // We rely on the client to filter by "room" ID
+    wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === 1) {
         client.send(msg.toString());
       }
